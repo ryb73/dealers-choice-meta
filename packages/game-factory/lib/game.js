108f3cdadaad59dedd4d29bb4a03ce481f3bd854
Ryan Biwer <rpbiwer@gmail.com>
2015-07-12 14:02:20 -0500

    extracted from dc-engine

---
+++ packages/game-factory/lib/game.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const BeginningState = require("dc-game-states").BeginningState,
+      GameData       = require("dc-engine").GameData;
+
+function Game($players, $deckConfig, $choiceProvider) {
+  let choiceProvider, gameData, state;
+
+  function initialize() {
+    choiceProvider = $choiceProvider;
+
+    gameData = new GameData($players, $deckConfig);
+    state = new BeginningState(gameData, choiceProvider);
+
+    $players = $deckConfig = $choiceProvider = null;
+  }
+
+  // Returns promise for boolean:
+  //  true if game should continue
+  //  false if game is over
+  function doNext() {
+    return state.go()
+      .then(function(newState) {
+        state = newState;
+        return !!state;
+      });
+  }
+  this.doNext = doNext;
+
+  Object.defineProperties(this, {
+    players: {
+      enumerable: true,
+      get: function() {
+        return gameData.players;
+      }
+    }
+  });
+
+  initialize();
+}
+
+module.exports = Game;
