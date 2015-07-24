3a06d6ca14031507a3c38b7c499067d6baf61b00
Ryan Biwer <rpbiwer@gmail.com>
2015-07-23 21:57:34 -0500

    initial commit

---
+++ packages/constants/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = {
+  TurnChoice: require("./lib/turn-choice"),
+  BuyFromAutoExchangeOption: require("./lib/buy-from-auto-exchange-option"),
+  MessageType: require("./lib/message-type"),
+  ResponseCode: require("./lib/response-code"),
+  RpsMoves: require("./lib/rps-moves"),
+  RpsConclusion: require("./lib/rps-conclusion"),
+};
