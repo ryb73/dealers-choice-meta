ce7c447ad3aaf7abcd1f10d5d20f2ca5b1fe52f2
Ryan Biwer <rpbiwer@gmail.com>
2015-08-14 21:29:53 -0500

    bunch of message types

--- packages/constants/lib/message-type.js
+++ packages/constants/lib/message-type.js
@@ -10,14 +10,30 @@ const MessageType = {
   Chat: "chat",
   Leave: "leave",
   Choice: "choice",
+  StartGame: "start-game",
 
   // S2C Commands
   RockPaperScissors: "rps",
   RpsCountdown: "rpsCountdown",
   RpsConclusion: "rpsConclusion",
 
-  ReplenishOption: "replenish",
-  ReplenishResult: "replenish-result"
+  GetTurnChoice: "get-turn-choice",
+  NotifyTurnChoice: "notify-turn-choice",
+
+  BuyFromExchangeOption: "replenish",
+  BuyFromExchangeResult: "replenish-result",
+
+  PromptForBids: "prompt-for-bids",
+
+  PromptOpenLot: "prompt-open-lot",
+  NotifyOpenLot: "notify-open-lot",
+
+  BeginBidding: "begin-bidding",
+  BiddingFinished: "bidding-finished",
+  NewHighBidder: "new-high-bidder",
+
+  PromptAcceptCounterOffer: "prompt-accept",
+  NotifyAcceptedOffer: "notify-accepted"
 };
 
 module.exports = MessageType;
