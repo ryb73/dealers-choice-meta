5af8e3e82b5227edbca77d067a9afcd2cea4fb53
Ryan Biwer <ryan@brightcellars.com>
2016-01-16 18:28:40 -0600

    Fix dir names

--- clone-all.sh
+++ clone-all.sh
@@ -1,10 +1,10 @@
-git clone https://github.com/ryb73/dealers-choice-card-pack.git
-git clone https://github.com/ryb73/dealers-choice-card-interfaces.git
-git clone https://github.com/ryb73/dealers-choice-engine.git
-git clone https://github.com/ryb73/dealers-choice-test.git
-git clone https://github.com/ryb73/dealers-choice-server.git
-git clone https://github.com/ryb73/dealers-choice-game-factory.git
-git clone https://github.com/ryb73/dealers-choice-game-states.git
-git clone https://github.com/ryb73/dealers-choice-front-end.git
-git clone https://github.com/ryb73/dealers-choice-game-ui.git
-git clone https://github.com/ryb73/dealers-choice-constants.git
+git clone https://github.com/ryb73/dealers-choice-card-pack.git card-pack
+git clone https://github.com/ryb73/dealers-choice-card-interfaces.git card-interfaces
+git clone https://github.com/ryb73/dealers-choice-engine.git engine
+git clone https://github.com/ryb73/dealers-choice-test.git test
+git clone https://github.com/ryb73/dealers-choice-server.git server
+git clone https://github.com/ryb73/dealers-choice-game-factory.git game-factory
+git clone https://github.com/ryb73/dealers-choice-game-states.git game-states
+git clone https://github.com/ryb73/dealers-choice-front-end.git front-end
+git clone https://github.com/ryb73/dealers-choice-game-ui.git game-ui
+git clone https://github.com/ryb73/dealers-choice-constants.git constants
