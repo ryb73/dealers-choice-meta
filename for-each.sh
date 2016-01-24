e3a8340bede7ade10eb5c04db2d9af514838a8bd
Ryan Biwer <ryan@brightcellars.com>
2016-01-24 15:43:56 -0600

    Generalized all-git-status.sh

--- for-each.sh
+++ for-each.sh
@@ -4,7 +4,7 @@ for path in ./*; do
   [ -d "${path}" ] || continue
   cd $path
   pwd
-  git status
+  eval $1
   [[ $? == 0 ]] || break
   sleep 0.3 # slow it down so I can see all of them; top is getting cut off in my console for some reason
   cd ..
