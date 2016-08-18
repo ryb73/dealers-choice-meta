7c9f60824b864e5c9dbdd473fe7710dbc48f8152
Ryan Biwer <ryan@brightcellars.com>
2016-08-17 21:35:06 -0500

    Presets menu

--- packages/db-interface/index.js
+++ packages/db-interface/index.js
@@ -7,35 +7,52 @@ let q          = require("q"),
 
 function createDbInterface() {
     let presets = [{
-      state: {
-        class: gameStates.PlayerTurnBeginState,
-        args: [ Symbols.player1 ]
-      },
-      players: [{
-        money: 17000,
-        dcCards: [ "list3", ..._.fill(new Array(4), Symbols.random) ],
-        cars: [ 24 , ..._.fill(new Array(3), Symbols.random) ],
-        insurances: [ "fire" ]
-      }, {
-        money: 17000,
-        dcCards: _.fill(new Array(5), Symbols.random),
-        cars: _.fill(new Array(4), Symbols.random),
-        insurances: [ Symbols.random ]
-      }]
+        id: 1,
+        name: "Basic Game",
+        state: {
+            class: gameStates.PlayerTurnBeginState,
+            args: [ Symbols.player1 ]
+        },
+        players: [{
+            money: 17000,
+            dcCards: [ "list3", ..._.fill(new Array(4), Symbols.random) ],
+            cars: [ 24 , ..._.fill(new Array(3), Symbols.random) ],
+            insurances: [ "fire" ]
+        }, {
+            money: 17000,
+            dcCards: _.fill(new Array(5), Symbols.random),
+            cars: _.fill(new Array(4), Symbols.random),
+            insurances: [ Symbols.random ]
+        }]
     }];
 
     function getPreset(id) {
         let defer = q.defer();
 
-        if(presets[id])
-            defer.resolve(presets[id]);
+        let preset = _.find(presets, { id });
+        if(preset)
+            defer.resolve(preset);
         else
             defer.reject(new Error("Invalid preset ID: " + id));
 
         return defer.promise;
     }
 
-    return { getPreset };
+    function getPresets() {
+        let defer = q.defer();
+
+        let presetData = presets.map((preset) => (
+            {
+                id: preset.id,
+                name: preset.name
+            }
+        ));
+        defer.resolve(presetData);
+
+        return defer.promise;
+    }
+
+    return { getPreset, getPresets };
 }
 
 module.exports = createDbInterface();
