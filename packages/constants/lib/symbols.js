0b4944fbabcad81b02161d95bf91d31e64911bcf
Ryan Biwer <ryan@brightcellars.com>
2016-08-20 23:32:05 -0500

    Presets in database

--- packages/constants/lib/symbols.js
+++ packages/constants/lib/symbols.js
@@ -5,5 +5,7 @@ module.exports = {
     player1: Symbol("player1"),
     player2: Symbol("player2"),
     player3: Symbol("player3"),
-    player4: Symbol("player4")
+    player4: Symbol("player4"),
+    player5: Symbol("player5"),
+    player6: Symbol("player6"),
 };
