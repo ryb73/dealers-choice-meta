88dda4090f7b1295a55eb035b4dc88c8c7b00931
Ryan Biwer <rpbiwer@gmail.com>
2016-11-13 20:52:34 -0600

    Spacing and stuff

--- packages/card-pack/lib/buy-from-auto-exchange-for-n.js
+++ packages/card-pack/lib/buy-from-auto-exchange-for-n.js
@@ -4,18 +4,18 @@ const q            = require("q"),
       giveUniqueId = require("give-unique-id");
 
 function BuyFromAutoExchangeForN(n) {
-  giveUniqueId(this);
+    giveUniqueId(this);
 
-  function play(gameData, choiceProvider, player) {
-    player.buyCar(gameData.carDeck.pop(), n);
-    return q();
-  }
-  this.play = play;
+    function play(gameData, choiceProvider, player) {
+        player.buyCar(gameData.carDeck.pop(), n);
+        return q();
+    }
+    this.play = play;
 
-  function canPlay(player, gameData) {
-    return gameData.carDeck.remaining >= 1 && player.money >= n;
-  }
-  this.canPlay = canPlay;
+    function canPlay(player, gameData) {
+        return gameData.carDeck.remaining >= 1 && player.money >= n;
+    }
+    this.canPlay = canPlay;
 }
 
 module.exports = BuyFromAutoExchangeForN;
