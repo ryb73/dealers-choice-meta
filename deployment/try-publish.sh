7b0d97faefcf3366b9b377e68cb1bd4a48a80240
Ryan Biwer <rpbiwer@gmail.com>
2016-12-14 02:06:51 -0500

    temp

--- deployment/try-publish.sh
+++ deployment/try-publish.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export NODE_ENV="" # Lerna bug should be fixed soon
+
 if lerna updated | grep -q "^-"; then
     echo "Publishing"
     lerna publish --yes --repo-version=$(node deployment/get-next-version.js)
