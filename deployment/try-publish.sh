4ae148021f87884505242262e24ab368ef312dfc
Ryan Biwer <rpbiwer@gmail.com>
2016-12-15 17:30:08 -0500

    temp

--- deployment/try-publish.sh
+++ deployment/try-publish.sh
@@ -2,9 +2,13 @@
 
 export NODE_ENV="" # Lerna bug should be fixed soon
 
-if lerna updated | grep -q "^-"; then
-    echo "Publishing"
-    lerna publish --yes --repo-version=$(node deployment/get-next-version.js)
-else
+if ! lerna updated | grep -q "^-"; then
     echo "Nothing to publish"
+    exit
 fi
+
+echo "Publishing"
+
+lerna publish --yes --repo-version=$(node deployment/get-next-version.js)
+
+ansible-galaxy install leonidas.nvm,v1.0.0
